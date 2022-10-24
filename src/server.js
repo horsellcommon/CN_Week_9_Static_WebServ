1098384480491d9791b64004c6a0c170ffc89a2d
horsellcommon <crawfordj716@gmail.com>
2022-10-24 11:07:56 +0100

    Server set up and index/homepage setup

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const app = express();
+
+// app.use(express.static);
+
+app.use("/static", express.static("public")); // Translates the /static dir and repoints it to the public directory, which houses the index - use() only routes to directories
+
+app.listen(5001, () => {
+    console.log("Connected, listening on port 5001")
+}) // 5001 for Mac compatibility
